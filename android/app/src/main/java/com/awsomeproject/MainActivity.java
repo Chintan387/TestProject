f8ac54c71d44668be7a09c121757aef38d3502e3
ChintanPersistent <50201804+Chintan387@users.noreply.github.com>
2019-05-14 18:22:10 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/awsomeproject/MainActivity.java
+++ android/app/src/main/java/com/awsomeproject/MainActivity.java
@@ -12,4 +12,29 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "AwsomeProject";
     }
+    
+    private void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }});
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new X509TrustManager(){
+                public void checkClientTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public void checkServerTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }}}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(
+                    context.getSocketFactory());
+            callApi();
+
+
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
+    }
 }
